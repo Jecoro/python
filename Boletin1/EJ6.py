b1b0e33c7e280548347e62632f815a1dd2779c17
Jecoro <jecora98@hotmail.com>
2021-11-02 19:54:28 +0100

    EJ6DONE

---
+++ Boletin1/EJ6.py
@@ -0,0 +1,18 @@
+ano_curso = int(input("Escribe el ano en curso: "))
+nombre_uno =input("Primer nombre: ")
+ano_uno = int(input("Primer ano: "))
+nombre_dos = input("Segundo nombre: ")
+ano_dos =int( input("Segundo ano: "))
+nombre_tres =input("Tercer nombre: ")
+ano_tres = int(input("Tercer ano: "))
+
+cumple_uno = ano_curso - ano_uno
+cumple_dos = ano_curso - ano_dos
+cumple_tres = ano_curso - ano_tres
+
+print(nombre_uno + " cumplira " + str(cumple_uno))
+print(nombre_dos + " cumplira " + str(cumple_dos))
+print(nombre_tres + " cumplira "+ str(cumple_tres))
+
+
+
